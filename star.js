fe01fe7ec5f24297a112fea4021e4a5aeea7c7ca
djcanvas <92258508+djcanvas@users.noreply.github.com>
2023-11-18 15:34:30 +0100

    Update star.js

--- star.js
+++ star.js
@@ -126,11 +126,11 @@ function step() {
 
 function update() {
 
-  velocity.tx *= 0.06;
-  velocity.ty *= 0.06;
+  velocity.tx *= 0.96;
+  velocity.ty *= 0.96;
 
-  velocity.x += ( velocity.tx - velocity.x ) * 0.8;
-  velocity.y += ( velocity.ty - velocity.y ) * 0.8;
+  velocity.x += ( velocity.tx - velocity.x ) * 0.1;
+  velocity.y += ( velocity.ty - velocity.y ) * 0.1;
 
   stars.forEach( ( star ) => {
 
